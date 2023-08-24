fbeb050aad827ba7ff0e4f4072381d9ffa57b47b
DoradoL <140122291+DoradoL@users.noreply.github.com>
2023-08-23 23:00:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var NOMBRE = "JOSE"
+var APELLIDO= "PEREZ"
+console.log(NOMBRE + " " + APELLIDO);
+var numero1= 10
+var numero2= 35
+var suma =( numero1 + numero2)
+console.log("el resultado de la suma es", suma)
+var numero3= 99
+console.log("el resultado de la suma anterior mas una tercer variable numerica da por resultado:", suma+numero3)
+
+var NOMBRE2 = "PEPE"
+var numero4 = 77
+console.log("la practica de sumar un string con un valor numerico da por resultado:", NOMBRE2+" "+numero4) */prueba suma string + numero */
+
+var validacion= true
+var numero5= 53
+console.log("el resultado de la divison entre un boolean y un factor numerico es:", validacion/numero5)
